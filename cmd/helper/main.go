7f8c67ee4fa1bcf1a6b4c075cccc2043f0059c22
Your Name <you@example.com>
2025-08-27 17:25:00 +0200

    Added

--- cmd/helper/main.go
+++ cmd/helper/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/username/firmware-builder/internal/build"
+)
 
 func main() {
 	fmt.Println("Helper-Tool gestartet")
+	build.SetupToolchain()
+	build.BuildBusybox()
+	build.BuildRootfs()
 }
